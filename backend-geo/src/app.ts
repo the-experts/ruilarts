bf9ce0e8eb96637841bbd805a9a1ab43655d5fb3
Jan Martijn Roetman <janmartijn.roetman@rws.nl>
2025-11-04 16:28:09 +0100

    Use local graphhopper

--- backend-geo/src/app.ts
+++ backend-geo/src/app.ts
@@ -20,22 +20,22 @@ app.get('/location', async (req, res) => {
     const queryAddress = address ? '%20and%20%27' + address + '%27' : ''
     console.log(queryAdressNumber)
     console.log(queryAddress)
-    const response = await fetch('https://api.pdok.nl/bzk/locatieserver/search/v3_1/free?q=' + postcode + queryAddress +queryAdressNumber + '&fl=id%20identificatie%20weergavenaam%20bron%20type%20openbareruimte_id%20nwb_id%20openbareruimtetype%20straatnaam%20straatnaam_verkort%20adresseerbaarobject_id%20nummeraanduiding_id%20huisnummer%20huisletter%20huisnummertoevoeging%20huis_nlt%20postcode%20buurtnaam%20buurtcode%20wijknaam%20wijkcode%20woonplaatscode%20woonplaatsnaam%20gemeentecode%20gemeentenaam%20provinciecode%20provincienaam%20provincieafkorting%20kadastrale_gemeentecode%20kadastrale_gemeentenaam%20kadastrale_sectie%20perceelnummer%20kadastrale_grootte%20gekoppeld_perceel%20kadastrale_aanduiding%20volgnummer%20gekoppeld_appartement%20wegnummer%20hectometernummer%20zijde%20hectometerletter%20waterschapsnaam%20waterschapscode%20rdf_seealso%20centroide_ll%20centroide_rd%20score&fq=type%3A%28adres%29&df=tekst&bq=type%3Aprovincie%5E1.5&bq=type%3Agemeente%5E1.5&bq=type%3Awoonplaats%5E1.5&bq=type%3Aweg%5E1.5&bq=type%3Apostcode%5E0.5&bq=type%3Aadres%5E1&start=0&rows=10&sort=score%20desc%2Csortering%20asc%2Cweergavenaam%20asc&wt=json'
+    const response = await fetch('https://api.pdok.nl/bzk/locatieserver/search/v3_1/free?q=' + postcode + queryAddress + queryAdressNumber + '&fl=id%20identificatie%20weergavenaam%20bron%20type%20openbareruimte_id%20nwb_id%20openbareruimtetype%20straatnaam%20straatnaam_verkort%20adresseerbaarobject_id%20nummeraanduiding_id%20huisnummer%20huisletter%20huisnummertoevoeging%20huis_nlt%20postcode%20buurtnaam%20buurtcode%20wijknaam%20wijkcode%20woonplaatscode%20woonplaatsnaam%20gemeentecode%20gemeentenaam%20provinciecode%20provincienaam%20provincieafkorting%20kadastrale_gemeentecode%20kadastrale_gemeentenaam%20kadastrale_sectie%20perceelnummer%20kadastrale_grootte%20gekoppeld_perceel%20kadastrale_aanduiding%20volgnummer%20gekoppeld_appartement%20wegnummer%20hectometernummer%20zijde%20hectometerletter%20waterschapsnaam%20waterschapscode%20rdf_seealso%20centroide_ll%20centroide_rd%20score&fq=type%3A%28adres%29&df=tekst&bq=type%3Aprovincie%5E1.5&bq=type%3Agemeente%5E1.5&bq=type%3Awoonplaats%5E1.5&bq=type%3Aweg%5E1.5&bq=type%3Apostcode%5E0.5&bq=type%3Aadres%5E1&start=0&rows=10&sort=score%20desc%2Csortering%20asc%2Cweergavenaam%20asc&wt=json'
     );
     //extract JSON from the http response
     if (response.ok) {
         let jsonPdok = await response.json();
         console.log(jsonPdok);
         let centroideLl = jsonPdok.response.docs[0].centroide_ll;
-        let converted1 =centroideLl.replace("POINT(", "")
-        converted1 =converted1.replace(")", "")
+        let converted1 = centroideLl.replace("POINT(", "")
+        converted1 = converted1.replace(")", "")
         const coords: string[] = converted1.split(" ")
         const jsonResponse = {
-            longitude : coords[0],
-            latitude : coords[1]
+            longitude: coords[0],
+            latitude: coords[1]
         }
         res.send(jsonResponse)
-    }else{
+    } else {
         console.log("location error happened")
     }
     res.send(response.statusText)
@@ -43,14 +43,11 @@ app.get('/location', async (req, res) => {
 
 /**
  * !! Please take care when calling it multiple times (include some delay..), grashopper rate limit 500 a day ?!!
- * example call1  http://localhost:3030/route?fromLon=5.03239361&fromLat=52.12244549&toLon=5.05239361&toLat=52.12244549
+ * example call1  http://localhost:4000/route?fromLon=5.03239361&fromLat=52.12244549&toLon=5.05239361&toLat=52.12244549
  * returns weight + distance
- *
  */
 app.get('/route', async (req, res) => {
     console.log('receiving')
-    const postcode = req.query.postcode
-
     const fromLon = parseFloat(req.query.fromLon)
     const fromLat = parseFloat(req.query.fromLat)
     const toLon = parseFloat(req.query.toLon)
@@ -82,7 +79,7 @@ app.get('/route', async (req, res) => {
         points_encoded: false
     });
     const response = await fetch(
-        'https://graphhopper.com/api/1/route?key=5c35b258-d4f2-4556-8df8-9da8d5011909',{
+        'https://graphhopper.com/api/1/route?key=5c35b258-d4f2-4556-8df8-9da8d5011909', {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
@@ -91,17 +88,83 @@ app.get('/route', async (req, res) => {
             body: body,
         }
     );
-    console.log(response)
-    //extract JSON from the http response
+
     if (response.ok) {
         let routeResponse = await response.json();
         const jsonResponse = {
-            time : routeResponse.paths[0].time,
-            distance : routeResponse.paths[0].distance
+            time: routeResponse.paths[0].time,
+            distance: routeResponse.paths[0].distance
         }
         res.send(jsonResponse)
-    }else{
-        console.log("route error happened")
+    } else {
+        console.log("route error happened: "+response)
+    }
+    res.send(response.statusText)
+});
+
+/**
+ * route local uses a local graphhopper
+ * example call1  http://localhost:4000/route?fromLon=5.03239361&fromLat=52.12244549&toLon=5.05239361&toLat=52.12244549
+ * returns weight + distance but can be extended by much more...!
+ */
+app.get('/route-local', async (req, res) => {
+    const fromLon = parseFloat(req.query.fromLon)
+    const fromLat = parseFloat(req.query.fromLat)
+    const toLon = parseFloat(req.query.toLon)
+    const toLat = parseFloat(req.query.toLat)
+    const transportProfile = req.query.profile
+    let body = JSON.stringify({
+        points: [
+            [
+                fromLon,
+                fromLat
+            ],
+            [
+                toLon,
+                toLat
+            ]
+        ],
+        profile: transportProfile ? transportProfile : 'car',
+        elevation: true,
+        instructions: true,
+        locale: "en_US",
+        points_encoded: true,
+        points_encoded_multiplier: 1000000,
+        details: [
+           "road_class",
+           "road_environment",
+            "max_speed",
+            "average_speed"
+        ],
+        snap_preventions: [
+            "ferry"
+        ],
+        timeout_ms: 10000,
+        alternative_route : {
+            max_paths: 3,
+        },
+        algorithm: "alternative_route"
+    });
+    const response = await fetch(
+        'http://localhost:8989/route?key=', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: body,
+        }
+    );
+
+    if (response.ok) {
+        let routeResponse = await response.json();
+        const jsonResponse = {
+            time: routeResponse.paths[0].time,
+            distance: routeResponse.paths[0].distance
+        }
+        res.send(jsonResponse)
+    } else {
+        console.log("route-local error happened : " +response )
     }
     res.send(response.statusText)
 });
