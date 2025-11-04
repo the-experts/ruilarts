c7d3f7e8f63f027e0d5f88e10509b40240941544
Jan Martijn Roetman <janmartijn.roetman@rws.nl>
2025-11-04 21:16:58 +0100

    fix small error

--- backend-geo/src/app.ts
+++ backend-geo/src/app.ts
@@ -42,8 +42,8 @@ app.get('/location', async (req, res) => {
         res.send(jsonResponse)
     } else {
         console.log("location error happened")
+        res.send(response.statusText)
     }
-    res.send(response.statusText)
 });
 
 /**
@@ -116,7 +116,7 @@ app.get('/route-local', async (req, res) => {
     const fromLat = parseFloat(req.query.fromLat)
     const toLon = parseFloat(req.query.toLon)
     const toLat = parseFloat(req.query.toLat)
-    const transportProfile = req.query.profile
+    const transportProfile = req.query.transportProfile
 
 
     let body = getBodyRouteApi(fromLon, fromLat, toLon, toLat, transportProfile);
@@ -156,11 +156,11 @@ app.get('/route-local', async (req, res) => {
  *
  */
 app.post('/post-code-distance', async (req, res) => {
+    const postCodeB = req.body.postCodeB
     const postCodeA = req.body.postCodeA
+
     const responseA = await fetch('https://api.pdok.nl/bzk/locatieserver/search/v3_1/free?q=' + postCodeA + '&fl=id%20identificatie%20weergavenaam%20bron%20type%20openbareruimte_id%20nwb_id%20openbareruimtetype%20straatnaam%20straatnaam_verkort%20adresseerbaarobject_id%20nummeraanduiding_id%20huisnummer%20huisletter%20huisnummertoevoeging%20huis_nlt%20postcode%20buurtnaam%20buurtcode%20wijknaam%20wijkcode%20woonplaatscode%20woonplaatsnaam%20gemeentecode%20gemeentenaam%20provinciecode%20provincienaam%20provincieafkorting%20kadastrale_gemeentecode%20kadastrale_gemeentenaam%20kadastrale_sectie%20perceelnummer%20kadastrale_grootte%20gekoppeld_perceel%20kadastrale_aanduiding%20volgnummer%20gekoppeld_appartement%20wegnummer%20hectometernummer%20zijde%20hectometerletter%20waterschapsnaam%20waterschapscode%20rdf_seealso%20centroide_ll%20centroide_rd%20score&fq=type%3A%28adres%29&df=tekst&bq=type%3Aprovincie%5E1.5&bq=type%3Agemeente%5E1.5&bq=type%3Awoonplaats%5E1.5&bq=type%3Aweg%5E1.5&bq=type%3Apostcode%5E0.5&bq=type%3Aadres%5E1&start=0&rows=10&sort=score%20desc%2Csortering%20asc%2Cweergavenaam%20asc&wt=json'
     );
-
-    const postCodeB = req.body.postCodeB
     const responseB = await fetch('https://api.pdok.nl/bzk/locatieserver/search/v3_1/free?q=' + postCodeB + '&fl=id%20identificatie%20weergavenaam%20bron%20type%20openbareruimte_id%20nwb_id%20openbareruimtetype%20straatnaam%20straatnaam_verkort%20adresseerbaarobject_id%20nummeraanduiding_id%20huisnummer%20huisletter%20huisnummertoevoeging%20huis_nlt%20postcode%20buurtnaam%20buurtcode%20wijknaam%20wijkcode%20woonplaatscode%20woonplaatsnaam%20gemeentecode%20gemeentenaam%20provinciecode%20provincienaam%20provincieafkorting%20kadastrale_gemeentecode%20kadastrale_gemeentenaam%20kadastrale_sectie%20perceelnummer%20kadastrale_grootte%20gekoppeld_perceel%20kadastrale_aanduiding%20volgnummer%20gekoppeld_appartement%20wegnummer%20hectometernummer%20zijde%20hectometerletter%20waterschapsnaam%20waterschapscode%20rdf_seealso%20centroide_ll%20centroide_rd%20score&fq=type%3A%28adres%29&df=tekst&bq=type%3Aprovincie%5E1.5&bq=type%3Agemeente%5E1.5&bq=type%3Awoonplaats%5E1.5&bq=type%3Aweg%5E1.5&bq=type%3Apostcode%5E0.5&bq=type%3Aadres%5E1&start=0&rows=10&sort=score%20desc%2Csortering%20asc%2Cweergavenaam%20asc&wt=json'
     );
 
@@ -171,11 +171,10 @@ app.post('/post-code-distance', async (req, res) => {
         let converted1 = centroideLl.replace("POINT(", "")
         converted1 = converted1.replace(")", "")
         const coords: string[] = converted1.split(" ")
-        const jsonResponse = {
+        return {
             longitude: coords[0],
             latitude: coords[1]
-        }
-        return jsonResponse;
+        };
     }
 
     if (responseA.ok && responseB.ok) {
